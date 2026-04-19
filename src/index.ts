6ae6225f860878c460fc64d86944685fdfaec433
aayushxjoshi <aayushj9173@gmail.com>
2026-04-19 23:30:45 +0530

    Init Repo

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/healthcheck", (req, res) => {
+    res.send({
+    	message: "Server Up and Runnning"
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server Up and Running at Port 3000")
+})
